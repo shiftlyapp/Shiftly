902565c46e1400d54805bd341334549c3ea659bf
Oshri Nuri <oshrinuri@gmail.com>
2018-11-24 19:43:14 +0200

    Add login activity animations and drawables

--- app/src/main/java/com/technion/shiftly/LoginActivity.java
+++ app/src/main/java/com/technion/shiftly/LoginActivity.java
@@ -1,7 +1,12 @@
 package com.technion.shiftly;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +14,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView welcome_text = (TextView) findViewById(R.id.welcome_header);
+        ImageView smiley = (ImageView)findViewById(R.id.smiley_pic);
+        Animation fade_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_anim);
+        welcome_text.startAnimation(fade_anim);
+        smiley.startAnimation(fade_anim);
     }
 }
