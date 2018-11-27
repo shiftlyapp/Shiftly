0df04f3142df2ff293a7b7d8e5362d2f0c264b3c
Gal Fleissig <fleissigal@gmail.com>
2018-11-27 10:36:21 +0200

    Create new empty activity for group creation #11

---
+++ app/src/main/java/com/technion/shiftly/GroupCreationActivity.java
@@ -0,0 +1,22 @@
+package com.technion.shiftly;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GroupCreationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+        startActivity(intent);
+    }
+}
