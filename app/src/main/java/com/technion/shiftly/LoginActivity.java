a84f9a35543ce958ee1051d24fe681e4d88a08f6
Oshri Nuri <oshrinuri@gmail.com>
2018-11-29 02:35:46 +0200

    Add animations and more drawables to login/signup pages (#13,#14)

--- app/src/main/java/com/technion/shiftly/LoginActivity.java
+++ app/src/main/java/com/technion/shiftly/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.technion.shiftly;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,11 +19,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView welcome_text = (TextView) findViewById(R.id.welcome_header);
-        ImageView smiley = (ImageView)findViewById(R.id.smiley_pic);
+        ConstraintLayout mLayout = (ConstraintLayout)findViewById(R.id.anim_bg);
+        AnimationDrawable animationDrawable = (AnimationDrawable)mLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.start();
+        ImageView user_pic = (ImageView)findViewById(R.id.user_pic);
         TextView signup_txt = (TextView)findViewById(R.id.new_to_our_app);
         Animation bounce_anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.bounce_anim);
         welcome_text.startAnimation(bounce_anim);
-        smiley.startAnimation(bounce_anim);
+        user_pic.startAnimation(bounce_anim);
         signup_txt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- app/src/main/java/com/technion/shiftly/SignupActivity.java
+++ app/src/main/java/com/technion/shiftly/SignupActivity.java
@@ -1,10 +1,11 @@
 package com.technion.shiftly;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.constraint.ConstraintLayout;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
-import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 
 public class SignupActivity extends AppCompatActivity {
@@ -13,6 +14,11 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ConstraintLayout mLayout = (ConstraintLayout)findViewById(R.id.signup_cl);
+        AnimationDrawable animationDrawable = (AnimationDrawable)mLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(1000);
+        animationDrawable.setExitFadeDuration(1000);
+        animationDrawable.start();
         ImageView signup_img = (ImageView)findViewById(R.id.signup_pic);
         ScaleAnimation t = new ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         t.setDuration((long) 1000);
