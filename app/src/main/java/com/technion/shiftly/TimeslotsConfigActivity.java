915a449643d46c79c09d91272cd9fb051dcb9d92
Gal Fleissig <fleissigal@gmail.com>
2018-12-10 17:21:24 +0200

    Create a list view for timeslots configuration #30

--- app/src/main/java/com/technion/shiftly/TimeslotsConfigActivity.java
+++ app/src/main/java/com/technion/shiftly/TimeslotsConfigActivity.java
@@ -3,13 +3,18 @@ package com.technion.shiftly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class TimeslotsConfigActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_timeslots_config);
+
+        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.timeslots_configure));
 
         Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
         startActivity(intent);
