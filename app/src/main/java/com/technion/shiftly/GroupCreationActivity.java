aa20a805a0d6e7da71fab55e693f4dcd7d0a181e
Gal Fleissig <fleissigal@gmail.com>
2018-12-10 12:29:23 +0200

    Improved Create Group form layout #11

--- app/src/main/java/com/technion/shiftly/GroupCreationActivity.java
+++ app/src/main/java/com/technion/shiftly/GroupCreationActivity.java
@@ -3,6 +3,7 @@ package com.technion.shiftly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -15,7 +16,12 @@ public class GroupCreationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_group_creation);
+        Toolbar mainToolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(mainToolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.group_create));
+
+
 
         Button continue_button = findViewById(R.id.continue_button);
         continue_button.setOnClickListener(new View.OnClickListener() {
