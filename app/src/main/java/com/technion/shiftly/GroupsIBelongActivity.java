23ed07325354d933ee80818818a215418f53d78e
Yakir <yakir.helets@gmail.com>
2018-12-10 14:04:51 +0200

    Add initial layout for "Groups I belong" (#10)

---
+++ app/src/main/java/com/technion/shiftly/GroupsIBelongActivity.java
@@ -0,0 +1,43 @@
+package com.technion.shiftly;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+
+public class GroupsIBelongActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    private String[] dataset;  //mockup data set
+    private TextView count;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_groups_i_belong);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        String num = Integer.toString(mAdapter.getItemCount());
+        count.setText("you have " + num + " groups!");
+        mLayoutManager.addView(count);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new GroupListItemAdapter(dataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+    // ...
+}
+
