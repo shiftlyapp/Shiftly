51b11e55e56ec5e5196057fee6fe0406e2d9c485
Oshri Nuri <oshrinuri@gmail.com>
2018-12-25 23:00:07 +0200

    Fix reloading issue after creating a new group (#26)

--- app/src/main/java/com/technion/shiftly/TimeslotsConfigActivity.java
+++ app/src/main/java/com/technion/shiftly/TimeslotsConfigActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ExpandableListView;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
