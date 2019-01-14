fccfac2dccc77aac6f7fc767421d4af49e23129b
Oshri Nuri <oshrinuri@gmail.com>
2019-01-15 01:19:16 +0200

    Add an empty view for GroupIManageActivity (when the User has 0 groups)

--- app/src/main/java/com/technion/shiftly/GroupsIBelongFragment.java
+++ app/src/main/java/com/technion/shiftly/GroupsIBelongFragment.java
@@ -4,12 +4,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -27,9 +25,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupsIBelongFragment extends Fragment {
 
