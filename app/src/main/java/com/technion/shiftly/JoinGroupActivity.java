5b986be8638134e550ae33d631c503b56e0318e8
Gal Fleissig <fleissigal@gmail.com>
2019-01-16 12:45:53 +0200

    Finish joining a group feature including all comments on git (#70)

--- app/src/main/java/com/technion/shiftly/JoinGroupActivity.java
+++ app/src/main/java/com/technion/shiftly/JoinGroupActivity.java
@@ -2,7 +2,10 @@ package com.technion.shiftly;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -27,8 +30,7 @@ import java.util.Map;
 import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
 import static java.lang.Math.toIntExact;
 
-// The first activity of the group creation process.
-// In this activity the future admin sets the group name.
+// The join group activity
 
 public class JoinGroupActivity extends AppCompatActivity {
 
@@ -36,6 +38,9 @@ public class JoinGroupActivity extends AppCompatActivity {
     private FirebaseUser currentUser;
     private DatabaseReference databaseRef;
     private List<String> groupsIds;
+    private CustomSnackbar mSnackbar;
+    private ConstraintLayout mLayout;
+    private Boolean group_exists_in_db;
 
     @Override
     public void onStart() {
@@ -64,6 +69,10 @@ public class JoinGroupActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join_group);
         mAuth = FirebaseAuth.getInstance();
+        mLayout = findViewById(R.id.join_group);
+        mSnackbar = new CustomSnackbar(CustomSnackbar.SNACKBAR_DEFAULT_TEXT_SIZE);
+        group_exists_in_db = false;
+
         Toolbar mainToolbar = findViewById(R.id.join_group_toolbar);
         setSupportActionBar(mainToolbar);
         getSupportActionBar().setTitle(getResources().getString(R.string.group_join_label));
@@ -81,48 +90,81 @@ public class JoinGroupActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (mAwesomeValidation.validate()) {
                     // Add the user to the group and close this intent
-                    String user_id = currentUser.getUid();
-                    final DatabaseReference userGroupsRef = databaseRef.child("Users").child(user_id);
                     final String group_code = ((EditText)findViewById(R.id.join_group_edittext)).getText().toString();
 
-                    userGroupsRef.addListenerForSingleValueEvent(new ValueEventListener() {
-                        // TODO: add: do this only if group not already in the groups
-
+                    // Check if group exists in the db
+                    databaseRef.child("Groups").addListenerForSingleValueEvent(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            // Increment groups number by one
+                            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                                String group = postSnapshot.getKey();
+                                if (group.equals(group_code)) {
+                                    group_exists_in_db = true;
+                                    break;
+                                }
 
-                            Map<String, Object> groups_count_map = new HashMap<>();
-                            Long groups_count = dataSnapshot.child("groups_count").getValue(Long.class);
-                            Long newValue = (groups_count + 1L);
-                            groups_count_map.put("groups_count", newValue);
-                            userGroupsRef.updateChildren(groups_count_map);
-
-
-                            // Add the group to the groups of the user
-                            for (DataSnapshot postSnapshot : dataSnapshot.child("groups").getChildren()) {
-                                groupsIds.add(postSnapshot.getValue(String.class));
                             }
+                            if (!group_exists_in_db) {
+                                mSnackbar.show(JoinGroupActivity.this, mLayout, getResources().getString(R.string.err_group_doesnt_exist), CustomSnackbar.SNACKBAR_ERROR,Snackbar.LENGTH_SHORT);
 
-                            Map<String, Object> groups_map = new HashMap<>();
-                            groupsIds.add(group_code);
-                            groups_map.put("groups", groupsIds);
-                            userGroupsRef.updateChildren(groups_map);
+                            } else {
+                                String user_id = currentUser.getUid();
+                                final DatabaseReference userGroupsRef = databaseRef.child("Users").child(user_id);
+                                userGroupsRef.addListenerForSingleValueEvent(new ValueEventListener() {
 
+                                    @Override
+                                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                                        // Get all of the groups that the user is a member of
+                                        for (DataSnapshot postSnapshot : dataSnapshot.child("groups").getChildren()) {
+                                            groupsIds.add(postSnapshot.getValue(String.class));
+                                        }
+
+                                        // Check if the user is already a member of this group
+                                        if (groupsIds.contains(group_code)) {
+                                            mSnackbar.show(JoinGroupActivity.this, mLayout, getResources().getString(R.string.err_group_already_exist), CustomSnackbar.SNACKBAR_ERROR,Snackbar.LENGTH_SHORT);
+                                        } else {
+
+                                            // Add this group to the user's groups list in the DB
+                                            Map<String, Object> groups_map = new HashMap<>();
+                                            groupsIds.add(group_code);
+                                            groups_map.put("groups", groupsIds);
+                                            userGroupsRef.updateChildren(groups_map);
+
+                                            // Increment the user's groups number by one
+                                            Map<String, Object> groups_count_map = new HashMap<>();
+                                            Long groups_count = dataSnapshot.child("groups_count").getValue(Long.class);
+                                            Long newValue = (groups_count + 1L);
+                                            groups_count_map.put("groups_count", newValue);
+                                            userGroupsRef.updateChildren(groups_count_map);
+
+                                            mSnackbar.show(JoinGroupActivity.this, mLayout, getResources().getString(R.string.group_join_success), CustomSnackbar.SNACKBAR_SUCCESS,Snackbar.LENGTH_SHORT);
+                                            Handler handler = new Handler();
+                                            handler.postDelayed(new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    // Switch back to the GROUPS_I_BELONG fragment after a short delay
+                                                    Intent intent = new Intent(getApplicationContext(), GroupListsActivity.class);
+                                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                                                    intent.putExtra("FRAGMENT_TO_LOAD",Constants.GROUPS_I_BELONG_FRAGMENT);
+                                                    startActivity(intent);
+                                                }
+                                            }, Constants.REDIRECTION_DELAY);
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                    }
+                                });
+                            }
                         }
-
                         @Override
                         public void onCancelled(@NonNull DatabaseError databaseError) {
 
                         }
                     });
-
-
-                    Intent intent = new Intent(getApplicationContext(), GroupListsActivity.class);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    intent.putExtra("FRAGMENT_TO_LOAD",Constants.GROUPS_I_BELONG_FRAGMENT);
-                    startActivity(intent);
-
 
                 }
             }
